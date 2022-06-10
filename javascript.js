e52ba37d338a60b6a6b1667b75b96c5e3a0888f4
chanpm26 <little26pc@yahoo.com>
2022-06-10 06:28:37 -0400

    Added minmax to JS and max-size for CSS container

--- javascript.js
+++ javascript.js
@@ -4,8 +4,8 @@ function createDiv(number) {
     for (let i = 0; i < (number*number); i++) {
         let div = document.createElement('div');
         container.appendChild(div);
-        container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${number}, minmax(7px, 700px))`;
+        container.style.gridTemplateRows = `repeat(${number}, minmax(7px, 700px))`;
     }
 }
 
